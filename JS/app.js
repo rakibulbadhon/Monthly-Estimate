7ff6ba5f5cc24d10b512eb50d8d2a1b7821d8d12
rakibul badhon <rakibulbadhon777@gmail.com>
2022-09-09 00:02:59 +0600

    second commit

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,30 @@
-document.getElementById('calculate-button').addEventListener('click', function () {
+//For get input field
+function getInputField(expense) {
+    const input = document.getElementById(expense + '-input');
+    const inputField = parseFloat(input.value);
 
-})
+    return inputField;
+}
+
+//For get amount field
+function getAmountField(amountId) {
+    const amountField = document.getElementById(amountId);
+
+    return amountField;
+}
+//Event handler for calculate balance
+document.getElementById('calculate-button').addEventListener('click', function () {
+    const incomeAmount = getInputField('income');
+    const foodAmount = getInputField('food');
+    const rentAmount = getInputField('rent');
+    const clothAmount = getInputField('cloth');
+
+    //Add all expenses   
+    const totalExpense = getAmountField('total-expense');
+    totalExpense.innerText = foodAmount + rentAmount + clothAmount;
+
+    //Get current balance
+    const balance = getAmountField('balance');
+    balance.innerText = incomeAmount - totalExpense.innerText;
+
+});
